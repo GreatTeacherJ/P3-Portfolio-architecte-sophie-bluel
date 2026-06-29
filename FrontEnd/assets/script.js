79ba255de5735a3a8e3a79e0a9b8c38abbd81f69
BLASCOJ24 <j.blasco69@gmail.com>
2026-06-30 00:11:13 +0200

    feature/ js ajout du filtre sur les boutons

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,7 +1,6 @@
 console.log("Démarage du script");
 
 worksRecovery();
-categoryRecovery();
 
 //Récupération de toutes la gallerie sur l'API et ajout de toutes la gallerie dans la page
 async function worksRecovery() {
@@ -10,7 +9,7 @@ async function worksRecovery() {
     const gallerieApi = await reponse.json();
 
     const galleryHtml = document.getElementById("gallery");
-
+    const set = new Set();
     for (let i = 0; i < gallerieApi.length; i++) {
       const workImg = document.createElement("img");
       const workTitle = document.createElement("figcaption");
@@ -18,12 +17,23 @@ async function worksRecovery() {
       workImg.src = gallerieApi[i].imageUrl;
       workTitle.textContent = gallerieApi[i].title;
 
-      const work = document.createElement("figure");
+      const work = document.createElement("figure"); //figure qui contient chaque travail
+      work.dataset.category = gallerieApi[i].category.name; //ajout d'un dataset pour la category pour le filtre
+
       work.appendChild(workImg);
       work.appendChild(workTitle);
 
       galleryHtml.appendChild(work);
+
+      set.add(gallerieApi[i].category.name); //ajout de la catergorie au set eviter les doublons
     }
+
+    categoryRecovery(set); //envoie du set pour la création des bouton de filtre
+
+    console.log(
+      "nombre de objets = ",
+      document.querySelectorAll("[data-category='Objets']").length,
+    );
   } catch (error) {
     console.log("Chargement de la gallerie echouée");
     console.error("Erreur : ", error);
@@ -31,29 +41,53 @@ async function worksRecovery() {
 }
 
 //Recuperation des catégorie et création d'un bouton de filtre par cattégorie
-async function categoryRecovery() {
-  try {
-    const reponse = await fetch("http://localhost:5678/api/categories");
-    const catergory = await reponse.json();
+function categoryRecovery(setCatergory) {
+  const button_container = document.querySelector(".button_container");
 
-    const button_container = document.querySelector(".button_container");
+  //Création d'un bouton et d'un listener pour chaque categorie
+  setCatergory.forEach((category) => {
+    const button = document.createElement("button");
+    button.textContent = category;
 
-    for (let i = 0; i < catergory.length; i++) {
-      const button = document.createElement("button");
-      button.textContent = catergory[i].name;
+    button_container.appendChild(button);
 
-      button_container.appendChild(button);
+    //Affiche toutes les figure qui on la bonne categorie et masque les autres
+    button.addEventListener("click", () => {
+      document.querySelectorAll("figure").forEach((fig) => {
+        if (fig.dataset.category === category) {
+          fig.classList.remove("hidden");
+        } else {
+          fig.classList.add("hidden");
+        }
+      });
 
+      desactivated_button(button);
+    });
+  });
 
-//ajout de l'écoute pour les boutons
-button.addEventListener("click", () =>{
+  //Ajout du listener pour le bouton tous
+  document.getElementById("button-all").addEventListener("click", (e) => {
+    document.querySelectorAll("figure").forEach((fig) => {
+      fig.classList.remove("hidden");
+    });
 
-console.log(button.textContent)
-
-})
-
-    }
-  } catch (error) {
-    console.error("Erreur : ", error);
-  }
+    desactivated_button(e.target);
+  });
 }
+
+//désactive le bouton activé
+function desactivated_button(button) {
+  const activeButton = document.querySelector(".button_active");
+
+  //Le bouton actif devient inactif
+  if (activeButton) {
+    activeButton.classList.remove("button_active");
+    activeButton.classList.add("button_inactive");
+  }
+
+  //Le bouton appuyer deviens actif
+  button.classList.add("button_active");
+  button.classList.remove("button_inactive");
+}
+
+function teste() {}
