ab5764ad8f9c2b0b380b1e51367cfefa0975b8d2
BLASCOJ24 <j.blasco69@gmail.com>
2026-06-29 19:07:08 +0200

    feature/ js : ajout de la fonction categroyRecovery html : ajout de la div button_container css : ajout du css pour les bouton filtre

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,9 +1,10 @@
 console.log("Démarage du script");
 
-worksRecuperation();
+worksRecovery();
+categoryRecovery();
 
 //Récupération de toutes la gallerie sur l'API et ajout de toutes la gallerie dans la page
-async function worksRecuperation() {
+async function worksRecovery() {
   try {
     const reponse = await fetch("http://localhost:5678/api/works");
     const gallerieApi = await reponse.json();
@@ -26,6 +27,24 @@ async function worksRecuperation() {
   } catch (error) {
     console.log("Chargement de la gallerie echouée");
     console.error("Erreur : ", error);
-    return;
+  }
+}
+
+//Recuperation des catégorie et création d'un bouton de filtre par cattégorie
+async function categoryRecovery() {
+  try {
+    const reponse = await fetch("http://localhost:5678/api/categories");
+    const catergory = await reponse.json();
+
+    const button_container = document.querySelector(".button_container");
+
+    for (let i = 0; i < catergory.length; i++) {
+      const button = document.createElement("button");
+      button.textContent = catergory[i].name;
+
+      button_container.appendChild(button);
+    }
+  } catch (error) {
+    console.error("Erreur : ", error);
   }
 }
