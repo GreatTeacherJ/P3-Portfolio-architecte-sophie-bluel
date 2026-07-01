1b0c1e6ca218856d4f0df2f3562bb0e91dd7ebc1
BLASCOJ24 <j.blasco69@gmail.com>
2026-07-01 19:24:19 +0200

    html css/mise en forme du mode edition js/réorganisation du code

--- FrontEnd/assets/script/main-page.js
+++ FrontEnd/assets/script/main-page.js
@@ -25,11 +25,6 @@ async function worksRecovery() {
     }
 
     categoryRecovery(set); //envoie du set pour la création des bouton de filtre
-
-    console.log(
-      "nombre de objets = ",
-      document.querySelectorAll("[data-category='Objets']").length,
-    );
   } catch (error) {
     console.log("Chargement de la gallerie echouée");
     console.error("Erreur : ", error);
@@ -88,29 +83,6 @@ function desactivated_button(button) {
 
 function teste() {}
 
-//Appel quand la page est chargées afin de vérifier si le mode
-// edition est activé en fonction du token du session storage
-function LisenPageLoad() {
-  document.addEventListener("DOMContentLoaded", () => {
-    //Récupération de la banniére édition
-    const editionBanner = document.querySelector(".edition-mode");
-
-    //Récupération du texte "login"
-    const login_aref = document.getElementById("login-text");
-
-    //si il y a le token alors on active le mode edition, affichage de la banniére
-    if (sessionStorage.getItem("token")) {
-      editionBanner.classList.remove("hidden");
-
-      login_aref.textContent = "Logout"; //le bouton login deviens logout
-    } else {
-      //si il n'y a pas de token on masque la banniére édition
-      editionBanner.classList.add("hidden");
-      login_aref.textContent = "Login"; //le bouton logout deviens login
-    }
-  });
-}
-
 //Quand on click sur logout le token est supprimer du storage
 // et la page est rechargée
 //i le bouton est login il est redirigé vers la page de log
@@ -120,11 +92,11 @@ function selectLog() {
   //si pas logé alors on va à la page de log
   if (login_aref.textContent === "Login") {
     window.location.href = "pages/login.html";
-  } else {//sinon on supprimer le token et on change le texte en login
+  } else {
+    //sinon on supprimer le token et on change le texte en login
     if (sessionStorage.getItem("token")) {
       //Suppression du token
       sessionStorage.removeItem("token");
-      login_aref.textContent = "Login";
     }
 
     //Recharge de la page, apelle de la fonction ListenPageLoad
@@ -132,11 +104,48 @@ function selectLog() {
   }
 }
 
-//Code lancer au démarrage
-console.log("Démarage du script");
+//Change le mode de la page entre edition et visiteur
+//si true le mode sera en edition
+//si false le mode sera en visiteur
+async function ModeVerification() {
+  await worksRecovery();
 
-worksRecovery();
-LisenPageLoad();
+  //Récupération de la banniére édition
+  const editionBanner = document.querySelector(".edition-mode");
+
+  //Récupération du texte "login"
+  const login_aref = document.getElementById("login-text");
+
+  const button_container = document.querySelector(".button_container");
+  const container_button_modif = document.querySelector(
+    ".container-button-modif",
+  );
+
+  //si il y a le token alors on active le mode edition, affichage de la banniére
+  //MODE EDITION
+  if (sessionStorage.getItem("token")) {
+    editionBanner.classList.remove("hidden"); //afficher la banniére edition
+
+    login_aref.textContent = "Logout"; //le bouton login deviens logout
+
+button_container.innerHTML = "";
+
+   
+
+    //MODE VISITEUR
+  } else {
+    editionBanner.classList.add("hidden"); //afficher la banniére edition
+
+    login_aref.textContent = "Login"; //le bouton login deviens logout
+
+    button_container.classList.remove("hidden");
+    container_button_modif.classList.add("hidden");
+  }
+}
+
+//Ajout de l'écoute quand la page est chargées afin de vérifier si le mode
+// edition est activé en fonction du token du session storage
+document.addEventListener("DOMContentLoaded", ModeVerification);
 
 //Ajout de l'écoute sur le texte de log, qui prend deux valeur login ou logout
 document.getElementById("login-text").addEventListener("click", selectLog);
